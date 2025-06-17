000b5254c2ce92b69e71f815f6cfe28bc0c7e29a
chetansource <chetan.141k@gmail.com>
2025-06-17 15:48:57 +0530

    resolved all svg icon errors

--- components/icones/Mail.tsx
+++ components/icones/Mail.tsx
@@ -12,14 +12,14 @@ const Mail = () => {
       <path
         d="M5.75 4.45001L10 7.42501L14.25 4.45001"
         stroke="white"
-        stroke-width="1.275"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.275"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M1.5 11.25V2.74999C1.5 1.8111 2.26112 1.04999 3.2 1.04999H16.8C17.7389 1.04999 18.5 1.8111 18.5 2.74999V11.25C18.5 12.1889 17.7389 12.95 16.8 12.95H3.2C2.26112 12.95 1.5 12.1889 1.5 11.25Z"
         stroke="white"
-        stroke-width="1.275"
+        strokeWidth="1.275"
       />
     </svg>
   );
