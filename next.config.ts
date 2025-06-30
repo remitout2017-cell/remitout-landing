852f99c488f7865b7a056f3dfb34f9cfa70a4804
chetansource <chetan.141k@gmail.com>
2025-06-30 14:14:43 +0530

    next config file is updated with the url hosted for backend

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,17 @@ const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
   images: {
-    domains: ["localhost"], // 👈 allow localhost
+    domains: ["localhost", "remitout-cms.vercel.app"], // 👈 allow localhost
     remotePatterns: [
       {
         protocol: "http",
         hostname: "localhost",
         port: "3001", // 👈 match your Payload server port
+        pathname: "/media/**",
+      },
+      {
+        protocol: "https",
+        hostname: "remitout-cms.vercel.app",
         pathname: "/media/**",
       },
     ],
